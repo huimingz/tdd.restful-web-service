317b5f2ef7d09e4b55f61b2b6db4b0ade8f932cc
huimingz <huimingz12@outlook.com>
2022-11-18 11:05:08 +0800

    ready

--- src/test/java/restful/tdd/ResourceServletTest.java
+++ src/test/java/restful/tdd/ResourceServletTest.java
@@ -181,8 +181,51 @@ public class ResourceServletTest extends ServletTest {
     }
 
     @Test
+    public void should_use_response_from_web_application_exception_thrown_by_providers_when_find_message_body_writer() {
+        WebApplicationException exception = new WebApplicationException(response().status(Response.Status.FORBIDDEN).build());
+        response().entity(new GenericEntity<>(2.5, Double.class), new Annotation[0]).returnFrom(router);
+        Mockito.when(providers.getMessageBodyWriter(eq(Double.class), eq(Double.class), eq(new Annotation[0]), eq(MediaType.TEXT_PLAIN_TYPE))).thenThrow(exception);
+        HttpResponse<String> httpResponse = get("/test");
+
+        Assertions.assertEquals(Response.Status.FORBIDDEN.getStatusCode(), httpResponse.statusCode());
+    }
+
+    @Test
+    public void should_map_exception_thrown_by_provides_when_find_message_body_writer() {
+        response().entity(new GenericEntity<>(2.5, Double.class), new Annotation[0]).returnFrom(router);
+        Mockito.when(providers.getMessageBodyWriter(eq(Double.class), eq(Double.class), eq(new Annotation[0]), eq(MediaType.TEXT_PLAIN_TYPE))).thenThrow(IllegalArgumentException.class);
+        Mockito.when(providers.getExceptionMapper(eq(IllegalArgumentException.class))).thenReturn(e -> response().status(Response.Status.FORBIDDEN).build());
+
+        HttpResponse<String> httpResponse = get("/test");
+
+        Assertions.assertEquals(Response.Status.FORBIDDEN.getStatusCode(), httpResponse.statusCode());
+    }
+
+    @Test
+    public void should_use_response_from_web_application_exception_thrown_by_message_body_writer() {
+        WebApplicationException exception = new WebApplicationException(response().status(Response.Status.FORBIDDEN).build());
+        response().entity(new GenericEntity<>(2.5, Double.class), new Annotation[0]).returnFrom(router);
+        Mockito.when(providers.getMessageBodyWriter(eq(Double.class), eq(Double.class), eq(new Annotation[0]), eq(MediaType.TEXT_PLAIN_TYPE))).thenReturn(new MessageBodyWriter<Double>() {
+            @Override
+            public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+                return false;
+            }
+
+            @Override
+            public void writeTo(Double aDouble, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) {
+                throw exception;
+            }
+        });
+
+        HttpResponse<String> httpResponse = get("/test");
+
+        Assertions.assertEquals(Response.Status.FORBIDDEN.getStatusCode(), httpResponse.statusCode());
+
+    }
+
+    @Test
     public void should_map_exception_thrown_by_message_body_writer() throws Exception {
-        new OutboundResponseBuilder().entity(new GenericEntity<>(2.5, Double.class), new Annotation[0]).returnFrom(router);
+        response().entity(new GenericEntity<>(2.5, Double.class), new Annotation[0]).returnFrom(router);
 
         Mockito.when(providers.getMessageBodyWriter(eq(Double.class), eq(Double.class), eq(new Annotation[0]), eq(MediaType.TEXT_PLAIN_TYPE))).thenReturn(new MessageBodyWriter<Double>() {
             @Override
